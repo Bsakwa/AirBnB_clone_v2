93b1a592f027eac35aa69fcf2c4b62d876e1d623
Bsakwa <sakwabrian23@gmail.com>
2023-03-20 11:45:58 +0000

    Added more unittests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,63 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Defines Unittests for models/city.py """
+
+import os
+import pycodestyle
+import models
+import MySQLdb
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
 from models.city import City
+from models.state import State
+from models.engine.file_storage import FileStorage
 
 
-class test_City(test_basemodel):
-    """ """
+class TestCity(unittest.TestCase):
+    """Unittests for testing the City class."""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
+    def test_pep8(self):
+        """Test pep8 styling."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        p = style.check_files(["models/city.py"])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_state_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+    def test_attributes(self):
+        """Check for attributes."""
+        ct = City()
+        self.assertEqual(str, type(ct.id))
+        self.assertEqual(datetime, type(ct.created_at))
+        self.assertEqual(datetime, type(ct.updated_at))
+        self.assertTrue(hasattr(ct, "name"))
+        self.assertTrue(hasattr(ct, "state_id"))
 
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+
+    def test_save_filestorage(self):
+        """Test save method with FileStorage."""
+        old = self.city.updated_at
+        self.city.save()
+        self.assertLess(old, self.city.updated_at)
+        with open("file.json", "r") as f:
+            self.assertIn("City." + self.city.id, f.read())
+
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+
+    def test_to_dict(self):
+        """Test to_dict method."""
+        city_dict = self.city.to_dict()
+        self.assertEqual(dict, type(city_dict))
+        self.assertEqual(self.city.id, city_dict["id"])
+        self.assertEqual("City", city_dict["__class__"])
+        self.assertEqual(self.city.created_at.isoformat(),
+                         city_dict["created_at"])
+        self.assertEqual(self.city.updated_at.isoformat(),
+                         city_dict["updated_at"])
+        self.assertEqual(self.city.name, city_dict["name"])
+        self.assertEqual(self.city.state_id, city_dict["state_id"])
+
+
+if __name__ == "__main__":
+    unittest.main()
