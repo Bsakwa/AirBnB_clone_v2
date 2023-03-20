bbeb8c463b08efc0e4fe87f72c7300d9a4447f76
Bsakwa <sakwabrian23@gmail.com>
2023-03-20 12:00:21 +0000

    Fix pycodestyle

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Defines Unittests for models/state.py """
+
+import os
+import pycodestyle
+import models
+import MySQLdb
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.city import City
 from models.state import State
+from models.engine.file_storage import FileStorage
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm import sessionmaker
 
 
-class test_state(test_basemodel):
-    """ """
-
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
-
-    def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+class TestState(unittest.TestCase):
+    """Unittests for testing the State class."""
