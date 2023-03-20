19b3bafc8ba06f67c7cac1359e6f78af623f1548
Bsakwa <sakwabrian23@gmail.com>
2023-03-20 12:14:43 +0000

    Add some more tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -17,3 +17,44 @@ from sqlalchemy.orm import sessionmaker
 
 class TestState(unittest.TestCase):
     """Unittests for testing the State class."""
+    def test_pep8(self):
+        """Test pep8 styling."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        p = style.check_files(["models/city.py"])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_attributes(self):
+        """Check for attributes."""
+        ct = State()
+        self.assertEqual(str, type(ct.id))
+        self.assertEqual(datetime, type(ct.created_at))
+        self.assertEqual(datetime, type(ct.updated_at))
+        self.assertTrue(hasattr(ct, "name"))
+
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_save_filestorage(self):
+        """Test save method with FileStorage."""
+        old = self.state.updated_at
+        self.state.save()
+        self.assertLess(old, self.city.updated_at)
+        with open("file.json", "r") as f:
+            self.assertIn("State." + self.state.id, f.read())
+
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_to_dict(self):
+        """Test to_dict method."""
+        city_dict = self.state.to_dict()
+        self.assertEqual(dict, type(state_dict))
+        self.assertEqual(self.state.id, city_state["id"])
+        self.assertEqual("State", state_dict["__class__"])
+        self.assertEqual(self.state.created_at.isoformat(),
+                         state_dict["created_at"])
+        self.assertEqual(self.state.updated_at.isoformat(),
+                         state_dict["updated_at"])
+        self.assertEqual(self.state.name, state_dict["name"])
+
+
+if __name__ == "__main__":
+    unittest.main()
