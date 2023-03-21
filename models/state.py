8c9d84d25f2b984fb81fe543ef8a5d8aa58f7dea
Bsakwa <sakwabrian23@gmail.com>
2023-03-21 09:03:33 +0000

    Add getters and table attributes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if storage_type == "db"
+    name = Column(String(128),
+                  nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """Returns a list of city instances"""
+            city_list = []
+            city_all = models.storage.all(City)
+            for city in city_all.values():
+                if city.state.id == self.id:
+                    city_list.append(city)
+            return city_list
