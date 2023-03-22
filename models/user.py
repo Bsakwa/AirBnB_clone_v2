69c1db55180e9f283ca2e66c08999e63046ff4c0
Bsakwa <sakwabrian23@gmail.com>
2023-03-22 08:29:31 +0000

    Mapped User class to mySQL

--- models/user.py
+++ models/user.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import models
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "users"
+        email = Column(String(128),
+                       nullable=False)
+        password = Column(String(128),
+                          nullable=False)
+        first_name = Column(String(128),
+                            nullable=False)
+        last_name = Column(String(128),
+                           nullable=False)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
+
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new User class"""
+        super().__init__(*args, **kwargs)
