7e7cd0e01a2a4dc944663ce93b25b4d92cba7630
Bsakwa <sakwabrian23@gmail.com>
2023-03-22 02:44:10 +0000

    Fixed pycodestyle

--- models/city.py
+++ models/city.py
@@ -3,13 +3,23 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128),
-                  nullable=False)
-    state_id = Column(String(60),
-                      ForeignKey('state_id'),
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128),
                       nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey("states.id"),
+                          nullable=False)
+    else:
+        name = ""
+        state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
