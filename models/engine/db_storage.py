d0bbbe0f03ab42911481760ec55f05270d62f535
Bsakwa <sakwabrian23@gmail.com>
2023-03-22 02:48:18 +0000

    Fixed pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines the new database storage for our models"""
 from os import getenv
-import models
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -10,14 +9,12 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
 
 
-class DBStorage():
+class DBStorage:
     """Defines our hbnb_dev_db"""
-    #Private class attributes
+    # Private class attributes
     __engine = None
     __session = None
 
@@ -25,7 +22,7 @@ class DBStorage():
         """Initialize the new database"""
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('localhost')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
@@ -36,20 +33,18 @@ class DBStorage():
 
     def all(self, cls=None):
         """Query the Database Session"""
-        all_dict = {}
-        for i in classes:
-            if cls is None or cls == i:
-                objs = self.__session.query(classes[i]).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    all_dict[key] = obj
-        return (all_dict)
+        if cls is None:
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
     def new(self, obj):
         """Add a new object to the database"""
-        if obj is not None:
-            self.__session.add(obj)
-            self.save()
+        self.__session.add(obj)
 
     def save(self):
         """Saves the changes made to the database"""
@@ -58,13 +53,38 @@ class DBStorage():
     def delete(self, obj=None):
         """delete obj if exists"""
         if obj is not None:
-            del obj
-            self.save()
+            self.session.delete(obj)
 
     def reload(self):
         """Creates all the tables from the database"""
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
         Base.metadata.create_all(self.__engine)
-        cur = sessionmaker(
-                            bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(cur)
-        self.session = Session()
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
