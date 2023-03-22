7ac93a49cd3b8432545dc2885a2b470d4ba5b6cf
Bsakwa <sakwabrian23@gmail.com>
2023-03-22 16:35:29 +0000

    Refactored

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ holds class Amenity"""
-import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
@@ -8,17 +7,6 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128),
-                      nullable=False)
-        place_amenities = relationship('Place', secondary='place_amenity')
-
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+class Amenity:
+    """Instantiates a new Amenity"""
+    name = ''
