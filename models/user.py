02c3885e0185829425bdbb6aaaddf35291a311d9
Bsakwa <sakwabrian23@gmail.com>
2023-03-23 00:36:17 +0000

    Instantiate a new user in the database

--- models/user.py
+++ models/user.py
@@ -16,10 +16,8 @@ class User(BaseModel, Base):
                        nullable=False)
         password = Column(String(128),
                           nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
     else:
         email = ''
         password = ''
