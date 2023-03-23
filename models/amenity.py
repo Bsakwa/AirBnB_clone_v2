e9e19c81b680aa8a8cdcb91289c9e32293d715a7
Bsakwa <sakwabrian23@gmail.com>
2023-03-23 00:13:00 +0000

    Fixed style issues

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-""" holds class Amenity"""
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class Amenity:
-    """Instantiates a new Amenity"""
-    name = ''
+class Amenity(BaseModel):
+    name = ""
