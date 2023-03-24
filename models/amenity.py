847a45fa44990ca4ca07a55bcc82302e25ea67b7
Bsakwa <sakwabrian23@gmail.com>
2023-03-24 17:46:03 +0000

    Fix a pycodestyle error

--- models/amenity.py
+++ models/amenity.py
@@ -9,8 +9,9 @@ import models
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity """
-    __tablename__ = 'amenities'
-    name = Column(String(128),
-                  nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity',
-                                   viewonly=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'amenities'
+        name = Column(String(128),
+                      nullable=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       viewonly=False,)
