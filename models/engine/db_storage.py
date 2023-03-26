2eeff9be1486cebc5fac3d5f9b31e9f4ada05fe6
Bsakwa <sakwabrian23@gmail.com>
2023-03-26 11:23:18 +0000

    Add a query for Amenity class

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -39,6 +39,7 @@ class DBStorage:
             objs.extend(self.__session.query(User).all())
             objs.extend(self.__session.query(Place).all())
             objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
 
         else:
             if type(cls) == str:
