2b61f66b8097d708a1906e5615f69643e0e54153
Bsakwa <sakwabrian23@gmail.com>
2023-04-06 15:25:12 +0000

    Trial code for deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -43,36 +43,38 @@ def do_deploy(archive_path):
         if not (path.exists(archive_path)):
             return False
 
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         # Upload the the archive to the /tmp directory of the server
         put(archive_path, '/tmp/')
 
         # Create the destination folder
-        time_stamp = archive_path[-18:-4]
-        run('mkdir -p /data/web_static/releases/web_static_{}/'.format
-            (time_stamp))
+        run('mkdir -p /data/web_static/releases/{}/'.format
+            (no_ext))
 
         # Uncompress the archive to the web server
-        run("tar -xzf /tmp/web_static_{}.tgz -C \
-            /data/web_static/releases/web_static_{}/".format
-            (time_stamp, time_stamp))
+        run("tar -xzf /tmp/{}.tgz -C \
+            /data/web_static/releases/{}/".format
+            (file_n, no_ext))
 
         # Delete the archive from the web server
-        run('rm /tmp/web_static_{}.tgz'.format(time_stamp))
+        run('rm /tmp/{}.'.format(file_n))
 
         # Delete the symbolic link from the web server
-        run("mv /data/web_static/releases/web_static_{}/web_static/* \
-            /data/web_static/releases/web_static_{}/".format
-            (time_stamp, time_stamp))
-        run("rm -rf /data/web_static/releases/web_static_{}/web_static"
-            .format(time_stamp))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format
+            (no_ext, no_ext))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(no_ext))
         run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link
-        run("ln -s /data/web_static/releases/web_static_{}/ \
-            /data/web_static/current".format(time_stamp))
+        run("ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(no_ext))
 
         print("New Version deployed!")
-    except Exception as e:
+        return True
+    except Exception:
         return False
 
 
