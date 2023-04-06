1b4e5eb853c01199a3027bf03f17ae3f547b5afd
Bsakwa <sakwabrian23@gmail.com>
2023-04-06 15:11:06 +0000

    Trial code for deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -39,25 +39,41 @@ def do_deploy(archive_path):
     Attr:
         archive_path (str): The path of our compressed archive
     """
-    if os.path.isfile(archive_path) is False:
-        return False
-
     try:
-        file_n = archive_path.split("/")[-1]
-        no_xt = file_n.split(".")[0]
-        s_path = "/data/web_static/releases/"
+        if not (path.exists(archive_path)):
+            return False
+
+        # Upload the the archive to the /tmp directory of the server
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(s_path, no_xt))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, s_path, no_xt))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(s_path, no_xt))
-        run('rm -rf {}{}/web_static'.format(s_path, no_xt))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(s_path, no_xt))
-        return True
+
+        # Create the destination folder
+        time_stamp = archive_path[-18:-4]
+        run('mkdir -p /data/web_static/releases/web_static_{}/'.format
+            (time_stamp))
+
+        # Uncompress the archive to the web server
+        run("tar -xzf /tmp/web_static_{}.tgz -C \
+            /data/web_static/releases/web_static_{}/".format
+            (time_stamp, time_stamp))
+
+        # Delete the archive from the web server
+        run('rm /tmp/web_static_{}.tgz'.format(time_stamp))
+
+        # Delete the symbolic link from the web server
+        run("mv /data/web_static/releases/web_static_{}/web_static/* \
+            /data/web_static/releases/web_static_{}/".format
+            (time_stamp, time_stamp))
+        run("rm -rf /data/web_static/releases/web_static_{}/web_static"
+            .format(time_stamp))
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link
+        run("ln -s /data/web_static/releases/web_static_{}/ \
+            /data/web_static/current".format(time_stamp))
+
+        print("New Version deployed!")
     except Exception as e:
         return False
-
 
 def deploy():
     """ Find the path and deploy"""
