f38ebabbfb73c59a924e9775faa1cad92b871a85
Bsakwa <sakwabrian23@gmail.com>
2023-04-21 08:25:23 +0000

    Refactored code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+"""This module starts a Flask web application"""
+
 from flask import Flask
 
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-
+    """This function returns a string when called upon"""
     return "Hello HBNB!"
