0266a12ff3b142b460500026e5f20729ce739c77
Bsakwa <sakwabrian23@gmail.com>
2023-04-21 08:27:06 +0000

    Refactored code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,8 +4,11 @@
 
 from flask import Flask
 
+if __name__ == "__main__":
+
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """This function returns a string when called upon"""
     return "Hello HBNB!"
+app.run(host='0.0.0.0', port=5000)
