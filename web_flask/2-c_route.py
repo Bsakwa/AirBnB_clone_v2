e5d49673cd16de6e7baf728717d41c1a3b0b5726
Bsakwa <sakwabrian23@gmail.com>
2023-04-21 08:44:53 +0000

    Fixed documentation and refactored

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 
-'''Starts a Flask web application'''
-
-from flask import Flask
-
-app = Flask(__name__)
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    '''Displays Hello HBNB!'''
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hb():
-    '''Displays HBNB'''
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    '''Displays C followed by the value of the variable text'''
-    return 'C {}'.format(text.replace('_', ' '))
-
+'''
+Starts a Flask web application
+Listens on 0.0.0.0 port 5000
+Routes:
+    /: Displays "Hello HBNB!"
+    /hbnb: Displays "HBNB"
+    /c/<text>: Displays "C" followed by the value of the variable text
+'''
 
 if __name__ == '__main__':
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def hello_hbnb():
+        '''Displays Hello HBNB!'''
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb')
+    def hb():
+        '''Displays HBNB'''
+        return 'HBNB'
+
+    @app.route('/c/<text>')
+    def c(text):
+        '''Displays C followed by the value of the variable text'''
+        return 'C {}'.format(text.replace('_', ' '))
+
     app.run(host='0.0.0.0', port=5000)
