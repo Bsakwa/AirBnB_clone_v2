d3463dd45baf666fec5e0d48f66fa02f49485238
Bsakwa <sakwabrian23@gmail.com>
2023-04-21 08:23:44 +0000

    Refactored code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    Routes and Displays Hello HBNB!
-    """
+
     return "Hello HBNB!"
