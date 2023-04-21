65312dc8ea5a3bdabc96f2cf03e97a0b5d49ee87
Bsakwa <sakwabrian23@gmail.com>
2023-04-21 08:53:57 +0000

    Fixed documentation and refactored

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,29 +11,26 @@ Routes:
 
 if __name__ == '__main__':
     from flask import Flask
-    
+
     app = Flask(__name__)
-    
+
     @app.route('/', strict_slashes=False)
     def hello_hbnb():
         '''Displays Hello HBNB!'''
         return 'Hello HBNB!'
-
 
     @app.route('/hbnb', strict_slashes=False)
     def hb():
         '''Displays HBNB'''
         return 'HBNB'
 
-
     @app.route('/c/<text>', strict_slashes=False)
     def c(text):
         '''Displays C followed by the value of the variable text'''
         return 'C {}'.format(text.replace('_', ' '))
 
-
     @app.route('/python', strict_slashes=False)
-    @app.route('/python/(<text>)', strict_slashes=False)
+    @app.route('/python/<text>', strict_slashes=False)
     def python(text='is cool'):
         '''Displays Python followed by a formatted text'''
         return 'Python {}'.format(text.replace('_', ' '))
