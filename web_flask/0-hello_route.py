faa068201d18e9911231e8a572efb8ad64f129b2
Bsakwa <sakwabrian23@gmail.com>
2023-04-21 08:11:14 +0000

    Refactored code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,6 +3,7 @@
 """
 Starts a Flask web application
 Listens on 0.0.0.0 port 5000
+Routes: /: display “Hello HBNB!”
 """
 
 from flask import Flask
@@ -12,10 +13,9 @@ app = Flask(__name__)
 def hello_hbnb():
     """
     Routes and Displays Hello HBNB!
-    on 0.0.0.0 port 5000
     """
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
